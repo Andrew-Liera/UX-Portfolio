3a1b382d1e53611ba9fe70c8cd3d31c8b300aa5d
Andrew-Liera <75274978+Andrew-Liera@users.noreply.github.com>
2021-07-12 15:54:34 -0400

    Commit
    Pretty much all of the landing page stuff, sans stylings

--- index.js
+++ index.js
@@ -35,3 +35,39 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(type, newTextDelay);
 });
+
+const card = document.querySelector('.skills-card');
+
+card.addEventListener('click', function() {
+    card.classList.toggle('is-flipped');
+});
+
+const card2 = document.querySelector('.skills-card-2');
+
+card2.addEventListener('click', function() {
+    card2.classList.toggle('is-flipped');
+});
+
+const card3 = document.querySelector('.skills-card-3');
+
+card3.addEventListener('click', function() {
+    card3.classList.toggle('is-flipped');
+});
+
+const card4 = document.querySelector('.pro-card');
+
+card4.addEventListener('click', function() {
+    card4.classList.toggle('is-flipped');
+});
+
+const card5 = document.querySelector('.pro-card-2');
+
+card5.addEventListener('click', function() {
+    card5.classList.toggle('is-flipped');
+});
+
+const card6 = document.querySelector('.pro-card-3');
+
+card6.addEventListener('click', function() {
+    card6.classList.toggle('is-flipped');
+});
